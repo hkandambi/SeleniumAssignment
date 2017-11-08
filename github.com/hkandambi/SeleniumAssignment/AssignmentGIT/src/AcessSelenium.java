0eb3c84defc01374566057374504c495ef33dee4
HKANDAMBI <HKANDAMBI@CL-HKANDAMBI.Virtusa.com>
2017-11-08 12:18:21 +0530

    initial code

---
+++ github.com/hkandambi/SeleniumAssignment/AssignmentGIT/src/AcessSelenium.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class AcessSelenium {
+	WebDriver driver;
+	
+	public void openBrowser(){
+		System.setProperty("webdriver.chrome.driver","./TestItems\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver(); 
+		driver.manage().window().maximize();
+		driver.get("http://www.seleniumhq.org/");
+	}
+	
+}
